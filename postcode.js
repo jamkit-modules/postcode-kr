b0aefc03408936417f7bbcdc6a8490f301d4195c
Hanyeol Cho <hanyeol.cho@gmail.com>
2021-02-25 08:21:57 +0900

    Fix the layout problem

--- postcode.js
+++ postcode.js
@@ -1,7 +1,14 @@
 function searchPostcode(onResult, onError) {
+    var layer = document.getElementById('layer');
+
     new daum.Postcode({ 
         oncomplete: function(result) { 
             onResult(result);
-        } 
-    }).embed();
+        },
+        onresize: function(size) {
+            layer.style.height = size.height + 'px';
+        },
+        width: '100%',
+        height: '100%'
+    }).embed(layer);
 }
